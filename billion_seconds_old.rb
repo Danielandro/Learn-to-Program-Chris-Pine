120406c185238cfc83a4a8d79ca5432ffa08a1e7
Daniel Ajilore <mtme13@hotmail.com>
2018-11-01 18:09:47 +0000

    add billion seconds calculator

---
+++ billion_seconds_old.rb
@@ -0,0 +1,18 @@
+puts "What year were you born? (Four digits are required)"
+year = gets.chomp
+
+puts "Which month were you born in? (Answer must be between 1-12)"
+month = gets.chomp
+
+puts "What day were you born on?"
+day = gets.chomp
+
+puts "What hour were you born in? (Use 24-hr time)"
+hour = gets.chomp
+
+puts "What minute were you born?"
+minute = gets.chomp
+
+puts "At how many seconds were you born?"
+seconds = gets.chomp
+
