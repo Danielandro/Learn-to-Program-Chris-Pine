64abe703c293eba84f32a92a9659e613b65e8b2d
Daniel Ajilore <mtme13@hotmail.com>
2019-03-12 17:03:18 +0000

    movie year exercise

---
+++ movie_year.rb
@@ -0,0 +1,9 @@
+movie_database =  {
+  :"Jaws" => 1975,
+  :"Shrek 2" => 2004,
+  :"Frozen" => 2013,
+  :"Harry Potter and the Sorcerer's Stone" => 2001,
+  :"Raiders of the Lost Ark" => 1981
+}
+
+movie_database.each { |movie, year| puts year }
