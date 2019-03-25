02eae1f5e5f493f4928a545ef2aa5964921f99b2
Daniel Ajilore <mtme13@hotmail.com>
2019-03-25 14:57:56 +0000

    correct solution

--- not_prime.rb
+++ not_prime.rb
@@ -1,15 +1,6 @@
 def not_primes(a,b)
-  sqrt_of_primes_less_than_20000 = [2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97, 101, 103, 107, 109, 113, 127, 131, 137, 139, 149]
-
-  num_range = (a...b).entries
-  answer = []
-
-  num_range.each do |num|
-    sqrt_of_primes_less_than_20000.each do |prime|
-      if num == prime next
-    end
-
-  end
+  require 'prime'
+  p (a..b).select{ |num| num.digits.all?(&:prime?) && !num.prime? }
 end
 
 not_primes(999, 2500)
